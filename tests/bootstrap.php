75c4a44a0ddae20ce5cb1f2a844fc1fc3d30bca7
Jacques Marneweck <jacques@powertrip.co.za>
2016-06-29 22:15:05 +0200

    Bootstrap for tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * PHP Validators
+ *
+ * @author    Jacques Marneweck <jacques@powertrip.co.za>
+ * @copyright 2015-2016 Jacques Marneweck.  All rights strictly reserved.
+ * @license   MIT
+ */
+
+require_once __DIR__ '/../vendor/autoload.php';
