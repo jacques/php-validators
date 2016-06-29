9df4dd8932e23517703ad6a172b7954f5509f4d1
Jacques Marneweck <jacques@powertrip.co.za>
2016-06-29 22:38:27 +0200

    ID validation

---
+++ src/Identification.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Identification Validation
+ *
+ * @author    Jacques Marneweck <jacques@powertrip.co.za>
+ * @copyright 2015-2016 Jacques Marneweck.  All rights strictly reserved.
+ * @license   MIT
+ */
+
+namespace Jacques\Validators;
+
+use Carbon\Carbon;
+
+class Identification {
+    /**
+     * Checks that a identification number is theoretically valid.  It does not
+     * validate that the identification number actually belongs to a human.
+     *
+     * id_type is either:
+     *   - 1 - South African Identification Number
+     *   - 2 - Passport
+     *   - 3 - South African Asylum Document Number
+     *
+     * @param string  $id_document_number Document Number of the document
+     * @param integer $id_type Identification Document Type
+     *
+     * @throws \InvalidArgumentException
+     *
+     * @return bool   true if is theoretically valid else false
+     */
+    public static function is_valid($id_document_number = null, $id_type = null) {
+        if (is_null($id_document_number)) {
+            throw new \InvalidArgumentException('Please enter a valid id document number.');
+        }
+
+        if (is_null($id_type)) {
+            throw new \InvalidArgumentException('Please pass in a valid id type for the id document number you are testing.');
+        }
+
+        if (!is_numeric($id_type)) {
+            throw new \InvalidArgumentException('Please pass in a numeric value for the id type wanting to be tested.');
+        }
+
+        $id_type = (int)$id_type;
+
+        if ($id_type < 1 || $id_type > 3) {
+            throw new \InvalidArgumentException('Please enter a numeric value for the id type.  1 == ZA ID / 2 == Passport / 3 == ZA Asylum');
+        }
+
+        if (1 == $id_type) {
+            if (!ctype_digit($id_document_number)) {
+                return false;
+            }
+
+            $idvalid = \PayBreak\Luhn\Luhn::validateNumber($id_document_number);
+
+            return ($idvalid);
+        }
+
+        return false;
+    }
+}
---
+++ tests/unit/IdentificationTest.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * PHP Validators
+ *
+ * @author    Jacques Marneweck <jacques@powertrip.co.za>
+ * @copyright 2015-2016 Jacques Marneweck.  All rights strictly reserved.
+ * @license   MIT
+ */
+
+namespace Jacques\Validators\Tests\Unit;
+
+use \Jacques\Validators\Identification;
+
+class IdentificationTest extends \PHPUnit_Framework_TestCase {
+    protected function setUp() {
+    }
+
+    protected function tearDown() {
+    }
+
+    /**
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionCode    0
+     * @expectedExceptionMessage Please enter a valid id document number.
+     */
+    public function testWithoutPassingArguments() {
+        $this->assertTrue(Identification::is_valid());
+    }
+
+    /**
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionCode    0
+     * @expectedExceptionMessage Please pass in a valid id type for the id document number you are testing.
+     */
+    public function testWithoutPassingIdType() {
+        $this->assertTrue(Identification::is_valid('SOMETHINGRANDOM'));
+    }
+
+    /**
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionCode    0
+     * @expectedExceptionMessage Please pass in a numeric value for the id type wanting to be tested.
+     */
+    public function testWithNonNumericIdType() {
+        $this->assertTrue(Identification::is_valid('SOMETHINGRANDOM', 'SOMETHINGRANDOM'));
+    }
+
+    /**
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionCode    0
+     * @expectedExceptionMessage Please enter a numeric value for the id type.  1 == ZA ID / 2 == Passport / 3 == ZA Asylum
+     */
+    public function testWithNumericIdTypeOfZero() {
+        Identification::is_valid('SOMETHINGRANDOM', '0');
+    }
+
+    /**
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionCode    0
+     * @expectedExceptionMessage Please enter a numeric value for the id type.  1 == ZA ID / 2 == Passport / 3 == ZA Asylum
+     */
+    public function testWithNumericIdTypeOfFour() {
+        $this->assertTrue(Identification::is_valid('SOMETHINGRANDOM', '4'));
+    }
+
+    /**
+     * @expectedException        \InvalidArgumentException
+     * @expectedExceptionCode    0
+     * @expectedExceptionMessage Please enter a numeric value for the id type.  1 == ZA ID / 2 == Passport / 3 == ZA Asylum
+     */
+    public function testWithNumericIdTypeOfNine() {
+        $this->assertTrue(Identification::is_valid('SOMETHINGRANDOM', '9'));
+    }
+
+    public function testValidSouthAfricanIdentificationNumbers() {
+        $this->assertTrue(Identification::is_valid('8510290194083', 1), 'Natalie Faye Webb - from EWN');
+    }
+
+    public function testInvalidIdentificationNumbers() {
+        $this->assertFalse(Identification::is_valid('2015-12-33', 1));
+        $this->assertFalse(Identification::is_valid('2015-12-33', 1));
+        $this->assertFalse(Identification::is_valid('MA123456', 1));
+
+        $this->assertFalse(Identification::is_valid('9007263637083', 1));
+    }
+};
